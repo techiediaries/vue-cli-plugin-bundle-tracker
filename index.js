ea9a93e903058e95e9a8fb7862ade17af6d28a09
Ahmed <techiediaries9@gmail.com>
2018-06-03 16:49:54 +0000

    Registering command

---
+++ index.js
@@ -0,0 +1,35 @@
+const BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = (api, options) => {
+  api.registerCommand('track', {
+    description: 'track Webpack bundles',
+    usage: 'vue-cli-service track',
+    options: {
+      '--path': `specify bundle path and filename`,
+      '--publicPath': `specify publicPath`,
+    }
+  },  (args) => {
+
+
+    const webpack = require('webpack')
+
+    const wpConfig = api.resolveWebpackConfig()
+    const path = args.path || './webpack-stats.json'
+
+    wpConfig.plugins.push(new BundleTracker({ path }))
+
+    if (args.publicPath) {
+      wpConfig.output.publicPath = args.publicPath
+    }
+
+    const wp = webpack(webpackConfig)
+    wp.watch({}, () => {})
+    
+    //wp.run()
+    
+  })
+}
+
+module.exports.defaultModes = {
+  track: 'development'
+}
